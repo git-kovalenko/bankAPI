73814c9a78794ec8ea81189932f467ac82e8faa5
a.kovalenko <a.kovalenko@ukr.net>
2018-01-31 22:21:56 +0200

    rate layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {PrivatBankService} from './services/privat-bank.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  exchangeRates: [] = [];
+  exchangeRates = [];
   constructor(
     private PBService: PrivatBankService
   ) {}
