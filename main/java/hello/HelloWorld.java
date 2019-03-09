6b163f7b25e9cd9b3e2c536e3b7a78878594591b
a.kovalenko <a.kovalenko@ukr.net>
2019-03-09 23:21:52 +0200

    maven config test

--- main/java/hello/HelloWorld.java
+++ main/java/hello/HelloWorld.java
@@ -1,7 +1,11 @@
 package hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
   public static void main(String[] args) {
+    LocalTime localTime = new LocalTime();
+    System.out.println("The local time is: " + localTime);
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
   }
