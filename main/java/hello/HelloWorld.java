a816ba5789a4a519ed4164d1ffc9509820cb3f34
a.kovalenko <a.kovalenko@ukr.net>
2019-03-10 20:28:30 +0200

    Spring boot app

--- main/java/hello/HelloWorld.java
+++ main/java/hello/HelloWorld.java
@@ -3,10 +3,10 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    LocalTime localTime = new LocalTime();
-    System.out.println("The local time is: " + localTime);
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
-  }
+//  public static void main(String[] args) {
+//    LocalTime localTime = new LocalTime();
+//    System.out.println("The local time is: " + localTime);
+//    Greeter greeter = new Greeter();
+//    System.out.println(greeter.sayHello());
+//  }
 }
