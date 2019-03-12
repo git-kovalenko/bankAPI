decc372ec783e516454c908de3c6fc8f7a131a1f
a.kovalenko <a.kovalenko@ukr.net>
2019-03-12 23:25:32 +0200

    Spring boot app

--- main/java/hello/HelloController.java
+++ main/java/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @RequestMapping("/")
+  @RequestMapping("/g")
   public String index() {
     return "Greetings from Spring Boot!!!";
   }
