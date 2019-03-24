aadb3034055fce909e7b3ec432b32bc6480b85de
a.kovalenko <a.kovalenko@ukr.net>
2019-03-25 00:28:22 +0200

    auth UI

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthService} from "./services/auth.service";
 
 
 @Component({
@@ -7,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    constructor(private app: AuthService, private http: HttpClient, private router: Router) {
+        this.app.authenticate(undefined, undefined);
+    }
+    logout() {
+        this.http.post('logout', {}).finally(() => {
+            this.app.authenticated = false;
+            this.router.navigateByUrl('/login');
+        }).subscribe();
+    }
+    ngOnInit () {
 
-  constructor(
-
-  ) {}
-  ngOnInit () {
-
-  }
+    }
 }
--- src/main/webapp/src/app/services/auth.service.ts
+++ src/main/webapp/src/app/services/auth.service.ts
@@ -5,9 +5,25 @@ import { Credentials } from '../login/interfaces';
 
 @Injectable()
 export class AuthService {
+  authenticated = false;
   constructor(
     private http: HttpClient
   ) {}
+
+  authenticate(credentials, callback) {
+    const headers = new HttpHeaders(credentials ? {
+      authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
+    } : {});
+
+    this.http.get('user', {headers: headers}).subscribe(response => {
+      if (response['name']) {
+        this.authenticated = true;
+      } else {
+        this.authenticated = false;
+      }
+      return callback && callback();
+    });
+  }
 
   public login(credentials: Credentials): Observable<any> {
     const httpOptions = {
